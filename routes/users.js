9e0f4351fd091f6f28f8066179b8510793b32af1
Hladun Yaroslav <hladunyaroslav@MacBook-Pro-Hladun.local>
2020-03-18 22:45:09 +0200

    made connection to local sql server

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mysql = require("mysql2");
 const router = express.Router();
+
+
+
+// SQL SERVER CONFIGURATION
+const connection = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   database: "event_calendar",
+   password: "qwertyqwerty123"
+});
+connection.connect(function(err){
+   if (err) return console.error("Помилка: " + err.message);
+   else console.log("Успішно підключено до SQL сервера");
+});
+
 
 router.get('/login', (req, res)=>res.render('login'));
 router.get('/register', (req, res)=>res.render('register'));
